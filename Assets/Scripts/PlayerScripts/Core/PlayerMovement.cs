238ebc47abc2dd2c01b19e86186b1e708db0f62f
Hoangdn1908 <hoangtranb02@gmail.com>
2026-07-08 13:50:30 +0700

    Update: Flip the player by the direction

--- Assets/Scripts/PlayerScripts/Core/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/Core/PlayerMovement.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public void Move(Vector2 direction, float currentSpeed)
     {
         rb.linearVelocity = direction * currentSpeed;
+        FlipPlayer(direction);
     }
 
     public void Stop()
     {
         rb.linearVelocity = Vector2.zero;
+    }
+
+    private void FlipPlayer(Vector2 direction) 
+    {
+        if (direction.x > 0f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else 
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
