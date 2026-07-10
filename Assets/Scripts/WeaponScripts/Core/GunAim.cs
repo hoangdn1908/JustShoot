4ba1ed7541e349d97432c284d90f0d33b21f36a9
Hoangdn1908 <hoangtranb02@gmail.com>
2026-07-10 14:49:21 +0700

    Fix: Aim gun flip relative to weapon position instead of world X

--- Assets/Scripts/WeaponScripts/Core/GunAim.cs
+++ Assets/Scripts/WeaponScripts/Core/GunAim.cs
@@ -16,23 +16,20 @@ public class GunAim : MonoBehaviour
 
     private void RotateWeapon() 
     {
+        if (cam == null)
+            cam = Camera.main;
+        if (cam == null) return;
         Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
+        mousePos.z = 0f;
         Vector2 dir = mousePos - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-        FlipWeapon(mousePos);
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        FlipWeapon(dir);
     }
 
-    private void FlipWeapon(Vector3 mousePos) 
+    private void FlipWeapon(Vector2 aimDirection)
     {
-        if (mousePos.x < 0)
-        {
-            transform.localScale = new Vector3(1, -1, 1);
-        }
-        else
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
+        float y = aimDirection.x < 0f ? -1f : 1f;
+        transform.localScale = new Vector3(1f, y, 1f);
     }
 }
