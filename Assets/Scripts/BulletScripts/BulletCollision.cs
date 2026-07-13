a68e383a1c1b1de6d0d3af5933d1164a54fc1c7c
Hoangdn1908 <hoangtranb02@gmail.com>
2026-07-13 14:18:12 +0700

    Update: Add damage for bullet

--- Assets/Scripts/BulletScripts/BulletCollision.cs
+++ Assets/Scripts/BulletScripts/BulletCollision.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class BulletCollision : MonoBehaviour
 {
     [SerializeField] private BulletController bulletController;
+    [SerializeField] private float damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
         if (collision.CompareTag("Enemy")) 
         {
-            bulletController.ReturnToPool();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+                bulletController.ReturnToPool();
+            }
         }
     }
 }
