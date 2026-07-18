d717edf98e97b2dd6814621326600e345878f26e
Hoangdn1908 <hoangtranb02@gmail.com>
2026-07-18 13:32:29 +0700

    Feat: Create shopUI and show total coin in shop

--- Assets/Scripts/CoinManager/PlayerWalletController.cs
+++ Assets/Scripts/CoinManager/PlayerWalletController.cs
@@ -1,7 +1,9 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerWalletController : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI coinText;
     public static PlayerWalletController Instance;
     private const string TotalCoinKey = "TotalCoin";
     public int TotalCoin;
@@ -26,6 +28,7 @@ public class PlayerWalletController : MonoBehaviour
     public void AddCoin(int amount) 
     {
         TotalCoin += amount;
+        UpdateCoinUI(TotalCoin);
         SaveCoin();
     }
 
@@ -38,5 +41,11 @@ public class PlayerWalletController : MonoBehaviour
     private void LoadCoin()
     {
         TotalCoin = PlayerPrefs.GetInt(TotalCoinKey, 0);
+        UpdateCoinUI(TotalCoin);
+    }
+
+    private void UpdateCoinUI(int amount) 
+    {
+        coinText.text = amount.ToString();
     }
 }
